bc16d79a1900e97067d200a77a7e792e6cc131b5
digvijaypatil07 <58438500+digvijaypatil07@users.noreply.github.com>
2021-12-10 00:18:53 +0530

    one more time

--- src/firstProject/MasterClass.java
+++ src/firstProject/MasterClass.java
@@ -7,7 +7,7 @@ public class MasterClass {
 
 	
 	public void test() {
-		System.out.println(" added changes from eclps and already made changes in master. and now i am going to push to master....seen");
+		System.out.println(" added changes from eclps and already made changes in master. and now i am going to push to master....seen...one more time");
 	}
 // added only in master branch not in feature branch. for check how PR merge. added changes in master from git. and i will make changes in eclips nd will try to add in master. lets see
 }
