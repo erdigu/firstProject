41b9d5dd6751880a569fbe8c7efdfebab2c041a2
digvijaypatil07 <58438500+digvijaypatil07@users.noreply.github.com>
2021-12-09 23:49:31 +0530

    written testmethod

--- src/firstProject/MasterClass.java
+++ src/firstProject/MasterClass.java
@@ -2,4 +2,10 @@ package firstProject;
 
 public class MasterClass {
 // added only in master branch not in feature branch. for check how PR merge
+
+
+	
+	public void test() {
+		System.out.println(" added changes from eclps and already made changes in master. and now i am going to push to master");
+	}
 }
