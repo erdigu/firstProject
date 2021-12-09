d7be08ac2e986ad5ca3ebbe0d9e0817559bdbd71
dsp <75614304+erdigprojects@users.noreply.github.com>
2021-12-09 23:44:40 +0530

    Update MasterClass.java

--- src/firstProject/MasterClass.java
+++ src/firstProject/MasterClass.java
@@ -1,5 +1,5 @@
 package firstProject;
 
 public class MasterClass {
-// added only in master branch not in feature branch. for check how PR merge
+// added only in master branch not in feature branch. for check how PR merge. added changes in master from git. and i will make changes in eclips nd will try to add in master. lets see
 }
